1a111628d83d340aeb8fa10899235fa5fc3b92a5
Viwu <sinconsistencia@gmail.com>
2025-12-11 18:04:18 -0600

    audio files

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.sound.FlxSound;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxG;
 import lime.app.Application;
 import lime.utils.Assets;
@@ -10,6 +12,28 @@ import flixel.FlxState;
 class PlayState extends FlxState
 {
 	public var song:SongJson;
+
+	public var songAudio:Array<FlxSound> = [];
+
+	public function playAudio()
+		for (audio in songAudio)
+			audio.play();
+
+	public function pauseAudio()
+		for (audio in songAudio)
+			audio.pause();
+
+	public function stopAudio()
+		for (audio in songAudio)
+			audio.stop();
+
+	public function setAudioTime(time:Float = 0)
+		for (audio in songAudio)
+			audio.time = time;
+
+	public function setAudioVolume(volume:Float = 1)
+		for (audio in songAudio)
+			audio.volume = volume;
 
 	override public function new(songName:String = 'test')
 	{
@@ -30,11 +54,22 @@ class PlayState extends FlxState
 			Application.current.window.alert('Song JSON Parsing Error: ' + e.message, 'Song JSON Parsing Error');
 			FlxG.switchState(() -> new PlayState('test'));
 		}
+
+		for (sound_file in song.metadata.sound_files)
+		{
+			var audio_file:FlxSound = new FlxSound();
+			audio_file.loadStream('assets/songs/' + songName + '/' + sound_file + '.wav');
+			songAudio.push(audio_file);
+		}
+
+		stopAudio();
 	}
 
 	override public function create()
 	{
 		super.create();
+
+		playAudio();
 	}
 
 	override public function update(elapsed:Float)
